3489f61b2800d480ea3ec9b22e7481d0a04d91d8
sivaraman <72295020+sivaramcap@users.noreply.github.com>
2022-01-27 00:30:47 +0530

    Add files via upload

---
+++ navtest.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("top").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("top").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
